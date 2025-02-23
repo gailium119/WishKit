787efd16a811bbedbdc579317451afb68ed66058
Administrator <Administrator>
2025-02-23 15:22:06 +0800

    Create Project Files

---
+++ WishKit.Classic/WinMain.cpp
@@ -0,0 +1,15 @@
+﻿#include <Windows.h>
+
+INT WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ INT nShowCmd)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
+    return 0;
+}
---
+++ WishKit.Core/DllMain.cpp
@@ -0,0 +1,22 @@
+﻿#include <Windows.h>
+
+extern "C" __declspec(dllexport) long _unused = 0;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    UNREFERENCED_PARAMETER(hModule);
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
